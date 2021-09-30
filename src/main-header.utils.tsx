90b20560a5e6edaeb91609e38ed6b203349ee771
danielkryska <daniel.kryska94@gmail.com>
2021-09-30 12:40:11 +0200

    Fix parsing empty urls

--- src/main-header.utils.tsx
+++ src/main-header.utils.tsx
@@ -108,12 +108,12 @@ export function getBtns(navBtnsConfig: any, filter = (config: any) => true) {
 
 function isBtnActive(btnsUrls: string[], btnUrl: string) {
   const currentUrlBase = location.protocol + "//" + location.hostname;
-  console.log(currentUrlBase, btnUrl, btnsUrls)
   if (!btnUrl.includes(currentUrlBase)) {
     return false;
   }
 
   const allBtnsSubpages = btnsUrls
+    .filter(url => !!url && url.trim() !== "")
     .map(url => new URL(url).pathname)
     .filter(path => path !== "/");
   return new URL(btnUrl).pathname === "/" && !allBtnsSubpages.includes(location.pathname)
